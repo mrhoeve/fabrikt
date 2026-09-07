578cedc6dbb115ba6336df983f7831ef1874c5c1
Marc Hoeve <mrhoeve@gmail.com>
2026-09-07 07:42:49 +0200

    Collect sequential parameter and header models

--- src/test/kotlin/com/cjbooms/fabrikt/parser/SourceHeaderModelSchemaCollectorTest.kt
+++ src/test/kotlin/com/cjbooms/fabrikt/parser/SourceHeaderModelSchemaCollectorTest.kt
@@ -31,6 +31,15 @@ class SourceHeaderModelSchemaCollectorTest {
             .isEqualTo("#/paths/~1subjects/get/responses/200/headers/Response-Metadata/content/application~1json/schema")
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = ["component", "response"])
+    fun `collects sequential header item schemas`(location: String) {
+        val componentHeader = location == "component"
+        val document = SourceOpenApiDocumentParser.parse(sequentialHeaderOpenApi(componentHeader))
+
+        assertThat(document.modelSchemas.keys).containsExactly("EventStreamItem")
+    }
+
     private fun openApi(version: String) =
         """
         openapi: $version
@@ -94,4 +103,45 @@ class SourceHeaderModelSchemaCollectorTest {
                     properties:
                       accepted: { type: boolean }
         """.trimIndent()
+
+    private fun sequentialHeaderOpenApi(componentHeader: Boolean): String =
+        if (componentHeader) {
+            """
+            openapi: 3.2.0
+            info:
+              title: Test
+              version: "1.0"
+            paths: {}
+            components:
+              headers:
+                Event-Stream:
+                  content:
+                    application/json-seq:
+                      itemSchema:
+                        type: object
+                        properties:
+                          eventId: { type: string }
+            """.trimIndent()
+        } else {
+            """
+            openapi: 3.2.0
+            info:
+              title: Test
+              version: "1.0"
+            paths:
+              /events:
+                get:
+                  responses:
+                    '200':
+                      description: Events
+                      headers:
+                        Event-Stream:
+                          content:
+                            application/json-seq:
+                              itemSchema:
+                                type: object
+                                properties:
+                                  eventId: { type: string }
+            """.trimIndent()
+        }
 }
